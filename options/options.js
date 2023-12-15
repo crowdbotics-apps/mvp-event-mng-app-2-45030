88b3d2816c8350fab1b3e0269908574a0f56df41
Bojan Petrusevski <bojan.petrusevski@crowdbotics.com>
2023-12-15 20:53:47 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "test-app-45030",
-  url: "https://test-app-45030.botics.co",
-  api: "https://test-app-45030.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "test-app-45030",
+  "url": "https://test-app-45030.botics.co",
+  "api": "https://test-app-45030.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "social-login"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
